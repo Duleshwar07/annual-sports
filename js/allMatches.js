c01c2bf50561b67c5835c45d368cc6f7032270dd
kjitendra <jitenparmar407@gmail.com>
2024-02-08 14:37:46 +0530

    - api implemented.

--- js/allMatches.js
+++ js/allMatches.js
@@ -42,58 +42,59 @@ $(document).ready(function () {
   }
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   // Function to fetch data from the API
   function playerData() {
-      $.ajax({
-          url: 'http://10.244.1.180:8080/events/3',
-          method: 'GET',
-          dataType: 'json', // Response type expected
-          success: function(response) {
-            console.log(">>>>>>>>>points", response[0]?.points)
-            let parseJSON = JSON.parse(response[0]?.points)
-            // Player names
-            $('#team1player1').text(response[0]?.team1[0].partName);
-            $('#team1player2').text(response[0]?.team1[1].partName);   
-            $('#team2player1').text(response[0]?.team2[0].partName);
-            $('#team2player2').text(response[0]?.team2[1].partName);
+    $.ajax({
+      url: "http://10.244.1.180:8080/events/3",
+      method: "GET",
+      dataType: "json", // Response type expected
+      success: function (response) {
+        console.log(
+          ">>>>>>>>>points city",
+          response[0]?.team1[1].teamMaster.teamName
+        );
+        let parseJSON = JSON.parse(response[0]?.points);
+        // Player names
+        $("#team1player1").text(response[0]?.team1[0].partName);
+        $("#team1player2").text(response[0]?.team1[1].partName);
+        $("#team2player1").text(response[0]?.team2[0].partName);
+        $("#team2player2").text(response[0]?.team2[1].partName);
 
-            // Players sets
-            if (Array.isArray(parseJSON)) {
-              parseJSON.forEach(function(teamData, teamIndex) {
-                  var teamName = teamData.name;
-                  var teamSets = teamData.sets;
-                  console.log("setsssss", teamSets)
-          
-                  // Update labels with team's sets points
-                  if (teamName === "Team 1") {
-                      updateLabels(teamSets, "#team1Set");
-                  } else if (teamName === "Team 2") {
-                      updateLabels(teamSets, "#team2Set");
-                  } else {
-                      console.log("Unknown team:", teamName);
-                  }
-              });
-          } else {
-              console.log("parseJSON is not in the expected format.");
-          }
-   
-          },
-          error: function(xhr, status, error) {
-              console.error(status + ': ' + error);
-          }
-      });
+        $("#city1players").text(response[0]?.team1[0].teamMaster.teamName);
+        $("#city2players").text(response[0]?.team2[1].teamMaster.teamName);
+
+        if (Array.isArray(parseJSON)) {
+          parseJSON.forEach(function (teamData, teamIndex) {
+            var teamName = teamData.name;
+            var teamSets = teamData.sets;
+            if (teamName === "Team 1") {
+              updateLabels(teamSets, "#team1Set");
+            } else if (teamName === "Team 2") {
+              updateLabels(teamSets, "#team2Set");
+            } else {
+              console.log("Unknown team:", teamName);
+            }
+          });
+        } else {
+          console.log("parseJSON is not in the expected format.");
+        }
+      },
+      error: function (xhr, status, error) {
+        console.error(status + ": " + error);
+      },
+    });
   }
   playerData();
 });
 
 function updateLabels(sets, labelPrefix) {
-    // Check if sets is an array
-    if (Array.isArray(sets)) {
-        sets.forEach(function(set, index) {
-            $(labelPrefix + (index + 1)).text(set.point);
-        });
-    } else {
-        console.log("Sets data is not an array.");
-    }
+  // Check if sets is an array
+  if (Array.isArray(sets)) {
+    sets.forEach(function (set, index) {
+      $(labelPrefix + (index + 1)).text(set.point);
+    });
+  } else {
+    console.log("Sets data is not an array.");
+  }
 }
